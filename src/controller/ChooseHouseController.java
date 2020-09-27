8ce7049ea4b656e0a641796174f11c45211ef219
jjjjjx <judsdeleonx@gmail.com>
2020-09-28 00:00:35 +0800

    Added CareerChoice fxml design

--- src/controller/ChooseHouseController.java
+++ src/controller/ChooseHouseController.java
@@ -2,7 +2,6 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
